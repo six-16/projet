0e15fb5544ba559dfd75a68719f41e46bae41043
pret <pret@p-region06.utt.fr>
2025-06-17 14:34:32 +0200

    encore modif

--- projet/app/view/connexion/inscription.php
+++ projet/app/view/connexion/inscription.php
@@ -61,6 +61,8 @@ require ($root . '/app/view/fragment/fragmentHeader.html');
         </div>
     </div>
     
-    <?php require_once '../fragment/fragmentFooter.html'; ?>
+    <?php 
+    require ($root . '/app/view/fragment/fragmentFooter.html');
+    ?>
 </body>
 </html>
